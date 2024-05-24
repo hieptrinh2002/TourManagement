a39093643c7d634214beb78bfb6cefdaaa1f5a2c
dungtm-1343 <tran.manh.dung@sun-asterisk.com>
2024-05-24 12:31:29 +0700

    Search and Filter

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,6 +1,26 @@
 class ToursController < ApplicationController
+  # before_action :search_upcoming, :filter_tours, only: :index
+
   def index
-    @pagy, @tours = pagy(Tour.upcoming,
-                         items: Settings.tour.items_per_page)
+    @tours = Tour.all
+    @tour_types = TourType.all
+    search
+    @pagy, @tours = pagy(@tours, items: Settings.tour.items_per_page)
+  end
+
+  private
+
+  def search
+    Tour::SEARCH_ATTRIBUTES.each do |field|
+      @tours = @tours.public_send "by_#{field}", params[field]
+    end
+    if params[:or]
+      @tours = @tours.by_city(params[:city])
+                     .or(@tours.by_tour_type_id(params[:tour_type_id]))
+    else
+      Tour::OPTION_ATTRIBUTES.each do |field|
+        @tours = @tours.public_send "by_#{field}", params[field]
+      end
+    end
   end
 end
